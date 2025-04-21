d40c251f17de9be2354a358c11ffe7dc3ae830a4
LuizadeJss <ljff_luz@hotmail.com>
2025-04-21 12:28:35 -0300

    Implemnetação do teste de verificação da quantidade de ingressos por pessoa (teste normal

--- test/test.c
+++ test/test.c
@@ -2,3 +2,17 @@
 #include "../src/venda-de-bilhetes.h"
 
 //Teste 1: Verifica se o tratamento para qntd de tikets está funcionando corretamente
+MU_TEST(test_calculo_normal){
+    int qntdTickets = verificarQntd(3);
+    mu_assert_double_eq(1, qntdTickets);
+}
+
+MU_TEST_SUITE(test_suite) {
+    MU_RUN_TEST(test_calculo_normal);
+}
+
+int main(int argc, char *argv[]) {
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}
