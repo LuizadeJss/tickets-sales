2bf764c163311d500d57ec8d507803798edb72e1
PedroHenriqueCos <phlkpedro@gmail.com>
2025-05-05 13:02:08 -0300

    integração continua

--- test/test.c
+++ test/test.c
@@ -42,13 +42,12 @@ MU_TEST(test_calculoPorIdade_abaixo){
     mu_assert_double_eq(-1, preco);
 }
 //Teste 8: Verifica de o tratamento para o cálculo de preço por idade com idade acima está funcionando corretamente
-MU_TEST(test_calculoPorIdade_acima){
-    
+MU_TEST(test_calculoPorIdade_acima){  
     double preco = calcularPrecoPorIdade(201, 3);
     mu_assert_double_eq(-1, preco);
 }
 
-//
+//Teste de integração continua
 
 MU_TEST_SUITE(test_suite) {
     MU_RUN_TEST(test_veriIngressos_normal);
